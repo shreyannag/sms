79ead8fee6942a7275bbc2868817791ca9f37aec
shreyannag <shreyannag@outlook.com>
2020-03-23 00:06:08 +0530

    WIP Day 4

--- register.php
+++ register.php
@@ -1,3 +1,13 @@
 <?php
-$name = $_POST[""]
+$schoolname = $_POST["regschoolname"];
+$schoolphone = $_POST["regschoolphone"];
+$schooladdress = $_POST["regschooladdress"];
+$schoolemail = $_POST["regschoolemail"];
+$password = $_POST["regpassword"];
+$question = $_POST["regquestion"];
+$answer = $_POST["reganswer"];
+
+
+echo $schooladdress."<br>".$schoolphone."<br>".$schoolemail."<br>".$schoolphone."<br>".$password."<br>".$answer;
+
 ?>
