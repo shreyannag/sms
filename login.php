2b31c8e18205857daf383b39e7b10d8925c21249
Shreyan Nag <shreyannag@outlook.com>
2020-03-25 10:57:25 +0530

    WIP Day 5

--- login.php
+++ login.php
@@ -1,5 +1,14 @@
+<?php
 include 'config.php';
 
 $loginName = $_POST["lname"];
-$loginemail = 
+$loginemail = $_POST["lemail"]; 
+$loginphone = $_POST["lphone"];
+$loginpassword = $_POST["lpassword"];
 
+$dNameSql = "SELECT schoolname from schooladmin";
+$dEmailSql = "SELECT schoolemail from schooladmin";
+$dPhoneSql = "SELECT schoolphone from schooladmin";
+$dPassword = "SELECT password from schooladmin";
+
+?>
